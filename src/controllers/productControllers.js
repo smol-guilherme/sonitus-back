324022fb30c0f1d65f74f430ae3a7a9e40ad9f7f
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-07-06 16:41:42 -0300

    Feature: Added skeleton to query all data types through a single function.

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,14 +1,56 @@
 import db from "../database/database.js";
+import { ObjectId } from "mongodb";
+
 const PRODUCTS_COLLECTION = process.env.MONGO_PRODUCTS_COLLECTION;
 
 export async function addItem(req, res) {
   const data = req.body;
   try {
-    const response = await db.collection(PRODUCTS_COLLECTION).insertOne({ ...data });
+    const response = await db
+      .collection(PRODUCTS_COLLECTION)
+      .insertOne({ ...data });
     res.status(201).send("Produto criado.");
     return;
   } catch (err) {
     res.send(err);
-    return err;
+    return;
+  }
+}
+
+export async function getItems(req, res) {
+  let params = req.query;
+  let query;
+  switch (true) {
+    case params.id !== undefined:
+      query = { _id: ObjectId(params.id) };
+      break;
+    case params.genre !== undefined:
+      console.log("bzzz");
+      query = { genre: params.genre };
+      break;
+    case params.id === undefined:
+    case params.genre === undefined:
+      query = {};
+      break;
+    default:
+      query = {};
+      break;
+  }
+  console.log(query);
+  try {
+    const response = await db
+      .collection(PRODUCTS_COLLECTION)
+      .aggregate([
+        { $match: { genre: query.genre } },
+        { $group: { _id: "$_id" } },
+        { $limit: 1 },
+      ])
+      .toArray();
+    console.log(response);
+    res.status(200).send(response);
+    return;
+  } catch (err) {
+    res.send(err);
+    return;
   }
 }
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addItem } from "../controllers/productControllers.js";
+import { addItem, getItems } from "../controllers/productControllers.js";
 import { findDuplicate } from "../middlewares/findDuplicate.js";
 
 const productRouter = Router();
 
 productRouter.post('/products', findDuplicate, addItem);
+productRouter.get('/products', getItems);
 
 export default productRouter;
