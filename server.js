697ff0c4ecf48ad80b4a22f62d400489c6885b5a
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-07-06 10:32:02 -0300

    Feature: Added '/' and '/health' routes for basic testing.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express,{ json } from "express";
 import cors from "cors";
+import router from "./src/routes/router.js";
 
 const app = express();
 const PORT_IN_USE = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(json());
+app.use(router)
 
 app.listen(PORT_IN_USE, () => console.log(`Server running from port ${PORT_IN_USE} @${Date().toString()}`));
--- src/routes/router.js
+++ src/routes/router.js
@@ -4,3 +4,5 @@ const router = Router();
 
 router.get('/', (req, res) => res.status(200).send("Welcome to the best vinyl store in the world"));
 router.get('/health', (req, res) => res.status(200).send("OK"));
+
+export default router;
