0f6c630942c8d80b0618a18d54fe0bcf12a3835e
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-07-11 17:22:05 -0300

    Refactor: Improved the e-mail message.

--- src/controllers/checkoutControllers.js
+++ src/controllers/checkoutControllers.js
@@ -26,7 +26,21 @@ export async function checkoutHandlers(req, res, next) {
     from: "sonitusstore@gmail.com", // Use the email address or domain you verified above
     subject: "Purchase info",
     text: `Thank you for buying with us `,
-    html: "<strong>See you next time!</strong>",
+    html: `
+    <div></div><strong><h1>Thank you for purchasing from us.</h1></strong>
+    <div></div>
+    <div></div>
+    <div><h3>Your order to the Address:</h3></div>
+    <div></div>
+    <div><p>${data.address},</p></div>
+    <div><h3>Your Order:</h3></div>
+    ${cart.map(item => `<div>${item.artist}, ${item.album}: ${item.quantity}</div>`)}
+    <div><h3>Is being processed, you will be informed by e-mail for delivery updates.</h3></div>
+    <div><h3>Remember, whenever you feel down, you can always listen to some vinyls.</h3></div>
+    <div><h3>Thank you for checking out Sonitus Vinyl Store and we hope to see you again soon.</h3></div>
+    <div></div>
+    <div><h2>Best regards,</h2></div>
+    <div><h2><strong>Sonitus Vinyl Store.</strong></h2></div>`,
   };
   try {
     await sgMail.send(msg);
@@ -37,7 +51,7 @@ export async function checkoutHandlers(req, res, next) {
       return res.sendStatus(500);
     }
   }
-  const purchase = "Successful";
+  const purchase = { data: "Successful" };
   res.status(200).send(purchase);
   return;
 }
