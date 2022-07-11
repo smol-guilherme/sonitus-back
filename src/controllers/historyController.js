3cfd44ba23cfd3ab7bfca496981b579211608474
Darlon Gomes <gomesdarlon@icloud.com>
2022-07-11 12:55:10 -0300

    update .env

--- src/controllers/historyController.js
+++ src/controllers/historyController.js
@@ -1,8 +1,6 @@
 import db from "../database/database.js";
 
-
 const PURCHASES_COLLECTION=process.env.MONGO_PURCHASES_COLLECTION
-const CARTS_COLLECTION=process.env.MONGO_CARTS_COLLECTION
 
 export async function searchHistory (req, res){
     const id = res.locals.id;
